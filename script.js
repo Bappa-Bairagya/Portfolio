1f4b0e2676f4c39b4550246389daa3d5c67b2a35
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-02-09 02:39:28 +0530

    cpmplete

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ function opentab(tabname){
 //    console.log(d);
    
 }
+
+var side=document.getElementById("side")
+
+function openmenu(){
+    side.style.right="0";
+}
+function closemenu(){
+    side.style.right="-200px";
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxZ6PQZu9AAyBQ-Wqld1Y_34fqW64z9EAhS_14uWo4Ig8dVkmku1ZoXSKYCXa_Mso9h/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg=document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML="Message sent succesfully"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
